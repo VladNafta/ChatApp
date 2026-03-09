fc08bce1f64bbc11475fa168579cbc49dd63b043
VladNafta <vladyslav.melnyk-ip232@nung.edu.ua>
2026-03-09 18:00:20 +0200

    add typescript

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import App from "./App.jsx";
+// import App from "./App.jsx";
 
 import HomePage from "./pages/Home/Home.jsx";
 import Header from "./components/Header/Header.jsx";
